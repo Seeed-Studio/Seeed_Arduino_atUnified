55f0b799928053f92f16e77e35abba9955816445
PowerfulCat <x4495@outlook.com>
2019-11-04 18:14:24 +0800

    Add a part of wifi at command.

--- at-script.cpp
+++ at-script.cpp
@@ -8,18 +8,18 @@ typedef std::string String;
 #include<stdio.h>
 #include<string.h>
 
-#define AT_AT           "AT"
-#define AT_RST          "AT+RST"
-#define AT_GMR          "AT+GMR"
-#define AT_ATE          "ATE"
-#define AT_GSLP         "AT+GSLP"
-#define AT_SLEEP        "AT+SLEEP"
-#define AT_UART_CUR     "AT+UART_CUR"
-#define AT_UART_DEF     "AT+UART_DEF"
-#define AT_SYSRAM       "AT+SYSRAM"
-#define AT_SYSFLASH     
-#define AT_FS           
-#define AT_RFPOWER      "AT+RFPOWER"
+#define AT              "AT"
+#define RST             "+RST"
+#define GMR             "+GMR"
+#define ATE             "ATE"
+#define GSLP            "+GSLP"
+#define SLEEP           "+SLEEP"
+#define UART_CUR        "+UART_CUR"
+#define UART_DEF        "+UART_DEF"
+#define SYSRAM          "+SYSRAM"
+#define SYSFLASH     
+#define FS           
+#define RFPOWER         "+RFPOWER"
 
 #define END_LINE        "\r\n"
 #define TOKEN           ":"
@@ -29,13 +29,14 @@ typedef std::string String;
 #define AT_READY        "ready"
 #define SET             "="
 #define ASK             "?"
+
 #define CMD(name,...)                       \
 inline bool name(__VA_ARGS__) {             \
     if (isNotWakeup()) return fail;         \
     if (atEcho(false) == fail) return fail; \
 
 #define $                return waitFlag(); }
-#define debug(...)       //Serial.printf(__VA_ARGS__);
+#define debug(...)       Serial.printf(__VA_ARGS__);
 
 constexpr bool fail = false;
 constexpr bool success = true;
@@ -66,11 +67,12 @@ bool atTest();
             return line;
         }
         line = line.substring(0, line.length() - 1);
-        debug(line.c_str());
+        //debug(line.c_str());
         return line;
     }
     void write(const char * value){
         com.print(value);
+        debug(value);
     }
     bool atBegin(){
         com.begin(115200);
@@ -95,31 +97,102 @@ bool waitFlag(){
     }
 }
 
-template<class ... arg>
-state ask(const char * fmt, arg ... list){
-    auto && resp = readLine();
+enum class hex_t : int32_t {};
 
-    if (sscanf(resp.c_str(), fmt + TOKEN_OFFSET, list...) == sizeof...(list)){
-        return ok;
+struct any{
+    any(int32_t * v) : v(v), isHex(false), skip(0){}
+    any(hex_t * v)   : v(v), isHex(true ), skip(0){}
+    any(String * v)  : v(v), isHex(false), skip(0){}
+    any(const char * v) :    isHex(false), skip(strlen(v)){}
+    any(char v) :    isHex(false), skip(1){}
+    template<class type> void set(type const & value){
+        *(type *)v = value;
     }
-    return over;
+    void *  v;
+    uint8_t skip;
+    bool    isHex;
+};
+
+template<class ... arg>
+bool subrx(const char * prefix, any * list){
+    String  resp = readLine();
+    auto    str = (char *)resp.c_str();
+    auto    len = strlen(prefix);
+
+    if (resp.indexOf(prefix) != 0){
+        return fail;
+    }
+
+    str += len;
+    
+    while(str[0]){
+        if (str[0] == ','){
+            str += 1;
+            continue;
+        }
+        if (list->skip){
+            str += list->skip;
+            list += 1;
+        }
+        if (str[0] == '\"'){
+            str += 1;
+            auto len = strchr(str, '\"') - str;
+            str[len] = '\0';
+            list->set<String>(str);
+            str += len + 1;
+        }
+        else{
+            auto end = str;
+            for (; isxdigit(end[0]); end++){
+                ;
+            }
+            sscanf(str, list->isHex ? "%x" : "%d", list->v);
+            str = end;
+        }
+
+        //DON't do this: for(;; list += 1)
+        list += 1;
+    }
+    return success;
 }
 
-void snd(){
-    write("\r\n");
+template<class ... arg>
+bool rx(char * prefix, arg ... list){
+    any     ls[] = { list... };
+    any   * v = ls;
+    return subrx(prefix, v);
 }
+
+enum class txMore{ };
+constexpr txMore more = txMore(0);
+
 void write(int32_t value){
     char buf[sizeof(int32_t) * 8];
     sprintf(buf, "%d", value);
     write(buf);
 }
-template<class first, class ... arg>
-void snd(first a, arg ... list){
-    write(a);
-    snd(list...);
+
+void write(String const & value){
+    write(value.c_str());
 }
 
-enum class resetMode{
+void tx(txMore){
+}
+
+void tx(){
+    write("\r\n");
+}
+
+template<class first, class ... arg>
+void tx(first a, arg ... list){
+    write(a);
+    if (sizeof...(list)){
+        write(",");
+    }
+    tx(list...);
+}
+
+enum class actionMode{
     waitReady,
     async,
 };
@@ -131,54 +204,50 @@ void waitReady(){
 }
 bool atEcho(bool enable);
 bool isNotWakeup(){
-    debug("isNotWakeup\n");
     if (needWaitWeekup == false){
         return false;
     }
-    debug("isNotWakeup\n");
     if (available()){
         waitReady();
         needWaitWeekup = false;
         return false;
     }
-    debug("return\n");
     return true;
 }
 
 CMD(atTest)
-    snd(AT_AT);
+    tx(AT);
 $
 
-CMD(atReset, resetMode mode = resetMode::waitReady)
-    snd(AT_RST);
-    if (mode == resetMode::waitReady){
+CMD(atReset, actionMode mode = actionMode::waitReady)
+    tx(AT RST);
+    if (mode == actionMode::waitReady){
         waitReady();
         return success;
     }
     needWaitWeekup = true;
 $
 
-class GMR{
+class FirmwareInfo{
 public:
-    char atVersion[16];
-    char sdkVersion[32];
-    char compileTime[32];
-    char binVersion[32];
+    String atVersion;
+    String sdkVersion;
+    String compileTime;
+    String binVersion;
 };
 
-CMD(atGMR, GMR * result)
-    constexpr int length = 4;
-    snd(AT_GMR);
-    if (ask(AT_AT "AT version:%s(", result->atVersion) == ok &&
-        ask(AT_AT "SDK version:%s", result->atVersion) == ok &&
-        ask(AT_AT "compile time:%s", result->compileTime) == ok &&
-        ask(AT_AT "Bin version:%s", result->binVersion) == ok
+CMD(atFirmwareInfo, FirmwareInfo * result)
+    tx(AT GMR);
+    if (rx("AT version:",   & result->atVersion)   == ok &&
+        rx("SDK version:",  & result->atVersion)   == ok &&
+        rx("compile time:", & result->compileTime) == ok &&
+        rx("Bin version:",  & result->binVersion)  == ok
     );
 $
 
 CMD(atDeepSleep, int32_t ms)
     needWaitWeekup = true;
-    snd(AT_GSLP SET, ms);
+    tx(AT GSLP SET, ms);
     return success;
 $
 
@@ -186,10 +255,10 @@ $
 //BUT -> bool atEcho(bool enable)
 bool atEcho(bool enable){
     //echo will be enable when reset
-    //THIS CMD NEED NOT 'SET' SUBFIX
+    //THIS CMD NEED NOT 'SET' SUFIX
     //AND DON'T USE CMD(atEcho, bool enable) FORMAT, IT WILL RESULT RECURRENCE.
     if (isNotWakeup()) return fail;
-    snd(AT_ATE, enable);
+    tx(enable ? ATE "1" : ATE "0"); 
     return waitFlag();
 }
 
@@ -203,36 +272,36 @@ public:
 };
 
 void uart(const char * header, AtUartConfig const & config){
-    snd(
-        header, SET, 
-        config.rate, ',', 
-        config.databits, ',',
-        config.stopbits, ',',
-        config.parity, ',', 
+    tx(
+        header,
+        config.rate, 
+        config.databits, 
+        config.stopbits, 
+        config.parity, 
         config.flowControl
     );
 }
 
 CMD(atUartTemp, AtUartConfig const & config)
-    uart(AT_UART_CUR, config);
+    uart(AT UART_CUR SET, config);
 $
 
 CMD(atUartNovolatile, AtUartConfig const & config)
-    uart(AT_UART_DEF, config);
+    uart(AT UART_DEF SET, config);
 $
 
 CMD(atSleepMode, int32_t enable)
-    snd(AT_SLEEP SET, enable);
+    tx(AT SLEEP SET, enable);
 $
 
 CMD(atSleepMode, int32_t * value)
-    snd(AT_SLEEP ASK);
-    ask(AT_SLEEP TOKEN "%d", value);
+    tx(AT SLEEP ASK);
+    rx(AT SLEEP TOKEN, value);
 $
 
 CMD(atRamSize, int32_t * bytes)
-    snd(AT_SYSRAM ASK);
-    ask(AT_SYSRAM TOKEN "%d", bytes);
+    tx(AT SYSRAM ASK);
+    rx(SYSRAM TOKEN, bytes);
 $
 
 // struct FlashPartition{
@@ -248,21 +317,21 @@ public:
 };
 
 CMD(atRfPower, int32_t wifi)
-    snd(AT_RFPOWER SET, wifi);
+    tx(AT RFPOWER SET, wifi);
 $
 
 CMD(atRfPower, RfPower const & config)
-    snd(AT_RFPOWER SET, 
-        config.wifiPower, ",",
-        config.bleAdvPower,  ",",
-        config.bleScanPower, ",",
+    tx(AT RFPOWER SET, 
+        config.wifiPower,
+        config.bleAdvPower,
+        config.bleScanPower,
         config.bleConnPower
     );
 $
 
 CMD(atRfPower, RfPower * config)
-    snd(AT_RFPOWER ASK);
-    ask(AT_RFPOWER TOKEN "%d,%d,%d,%d", 
+    tx(AT RFPOWER ASK);
+    rx(RFPOWER TOKEN, 
         & config->wifiPower,
         & config->bleAdvPower,
         & config->bleScanPower,
@@ -270,16 +339,19 @@ CMD(atRfPower, RfPower * config)
     );
 $
 
-#define AT_CWMODE       "AT+CWMODE"
-#define AT_CWJAP        "AT+CWJAP"
-#define AT_CWLAP        "AT+CWLAP"
+#define CWMODE       "+CWMODE"
+#define CWJAP        "+CWJAP"
+#define CWLAP        "+CWLAP"
+#define CWQAP        "+CWQAP"
+#define CWSAP        "+CWSAP"
+#define CWLIF        "+CWLIF"
 
 CMD(atWifiMode, int32_t mode)
-    snd(AT_CWMODE SET, mode);
+    tx(AT CWMODE SET, mode);
 $
 CMD(atWifiMode, int32_t * mode)
-    snd(AT_CWMODE ASK);
-    ask(AT_CWMODE TOKEN, mode);
+    tx(AT CWMODE ASK);
+    rx(CWMODE TOKEN, mode);
 $
 
 class WifiLinkInfo{
@@ -296,23 +368,22 @@ CMD(atWifiConnect,
     const char * pwd, 
     const char * bssid = "")
     if (bssid == ""){
-        snd(AT_CWJAP SET,  ssid, ",", pwd);
+        tx(AT CWJAP SET, ssid, pwd);
     }
     else{
-        snd(AT_CWJAP SET, ssid, ",", pwd, ",", bssid);
+        tx(AT CWJAP SET, ssid, pwd, bssid);
     }
 $
 
 template<class callback>
-CMD(atWifiConnect, callback && call)
-    String token = AT_CWLAP TOKEN + TOKEN_OFFSET;
+CMD(atWifiScan, callback && call)
     String current;
-    snd(AT_CWLAP);
+    tx(AT CWLAP);
     while((current = readLine()).length() == 0){
         delay(1);
     }
-    
-    while (current.indexOf(token) != -1){
+    while (current.indexOf(CWLAP TOKEN) != -1){
+
         //char    line[] = "+CWLAP:(3,\"POT\",-AL00a\",-53,\"12:34:56:78:9a:bc\",1)";
         char *  line = (char *)current.c_str();
         char *  p = NULL;
@@ -321,16 +392,17 @@ CMD(atWifiConnect, callback && call)
         int32_t ecn;
         int32_t rssi;
         int32_t channel;
+
         p = strrchr(line, ',');
         p[0] = '\0';
 
-        for (size_t i = 0; i < 2; i++) {
+        for (size_t i = 0, length = strlen(line); length-- > 0; ) {
             t = p;
             p = strrchr(line, ',');
             p[0] = '\0';
             t[0] = ',';
         }
-        sscanf(line, "+CWLAP:(%d,", & ecn);
+        sscanf(line, CWLAP TOKEN "(%d,", & ecn);
         sscanf(p + 1, "%d,\"%x:%x:%x:%x:%x:%x\",%d", 
             & rssi,
             & mac[0],
@@ -347,7 +419,6 @@ CMD(atWifiConnect, callback && call)
         p[-1] = '\0';
 
         WifiLinkInfo info;
-
         info.ecn = ecn;
         info.ssid = strchr(line, ',') + 2; //skip ',' '\"'
         info.rssi = rssi;
@@ -357,5 +428,80 @@ CMD(atWifiConnect, callback && call)
         info.channel = channel;
         call(info);
         current = readLine();
+    }
+$
+
+CMD(atWifiDisconnect)
+    tx(AT CWQAP);
+$
+
+class WifiApConfigure{
+public:
+    String  ssid;
+    String  pwd;
+    int32_t channel;
+    int32_t ecn;
+    int32_t maxConnect;
+    int32_t isSsidHidden;
+    WifiApConfigure() : 
+        maxConnect(-1),
+        isSsidHidden(-1){
+    }
+};
+
+CMD(atWifiApConfigure, WifiApConfigure * configure)
+    tx(AT CWSAP ASK);
+    rx(CWSAP TOKEN, 
+        & configure->ssid,
+        & configure->pwd,
+        & configure->channel,
+        & configure->ecn,
+        & configure->maxConnect,
+        & configure->isSsidHidden
+    );
+$
+
+CMD(atWifiApConfigure, WifiApConfigure const & configure)
+    tx(AT CWSAP SET,
+        configure.ssid,
+        configure.pwd,
+        configure.channel,
+        configure.ecn,
+        more
+    );
+    if (configure.maxConnect == -1){
+        tx();
+    }
+    else if (configure.isSsidHidden == -1){
+        tx(configure.maxConnect);
+    }
+    else{
+        tx(configure.maxConnect, configure.isSsidHidden);
+    }
+$
+
+class WifiUserList{
+public:
+    uint8_t ip[4];
+    uint8_t mac[6];
+};
+
+template<class callback>
+CMD(atWifiUserList, callback && call)
+    int32_t ip[4];
+    hex_t   mac[6];
+    tx(AT CWLIF);
+    while (rx(CWLIF TOKEN, 
+        ip + 0, '.', ip + 1, '.', ip + 2, '.', ip + 3,
+        mac + 0, ':', mac + 1, ':', mac + 2, ':', mac + 3, ':', mac + 4, ':', mac + 5
+    ) == success){
+        WifiUserList user;
+        for (size_t i = 0; i < 4; i++){
+            user.ip[i] = ip[i];
+        }
+        for (size_t i = 0; i < 6; i++){
+            user.mac[i] = (uint8_t)mac[i];
+        }
+        call(user);
     }
 $
