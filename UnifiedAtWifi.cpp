d380797d3428caabd92b3e9124fd2f7c9b316635
PowerfulCat <x4495@outlook.com>
2019-11-11 14:03:46 +0800

    Use the sscanf/sprintf style function to reduce line of code.

--- UnifiedAtWifi.cpp
+++ UnifiedAtWifi.cpp
@@ -1,67 +1,106 @@
 #include"UnifiedAtWifi.h"
 
-#define CWMODE       "+CWMODE"
-#define CWJAP        "+CWJAP"
-#define CWLAP        "+CWLAP"
-#define CWQAP        "+CWQAP"
-#define CWSAP        "+CWSAP"
-#define CWLIF        "+CWLIF"
-#define CWDHCP       "+CWDHCP"
-#define CWDHCPS      "+CWDHCPS"
-#define CWAUTOCONN   "+CWAUTOCONN"
-#define CWSTARTSMAR  "+CWSTARTSMAR"
-#define CWSTOPSMART  "+CWSTOPSMART"
-#define WPS          "+WPS"
-#define CWHOSTNAME   "+CWHOSTNAME"
-#define MDNS         "+MDNS"
+#define SET_CWMODE              "AT+CWMODE=%d", a0
+#define ASK_CWMODE              "AT+CWMODE?"
+#define GET_CWMODE              "+CWMODE:%d", a0
 
-CMD(atWifiMode, int32_t mode)
-    tx(AT CWMODE SET, mode);
+#define SET_CWJAP               "AT+CWJAP=%s,%s%+s", a0.ssid, a0.pwd, a0.bssid
+#define ASK_CWJAP               "AT+CWJAP?"
+#define GET_CWJAP               "+CWJAP:%s,%s,%d,%d", a0->ssid, a0->bssid, a0->channel, a0->rssi
+
+#define ASK_CWLAP               "AT+CWLAP?"
+#define GET_CWLAP               "+CWLAP:"
+
+#define SET_CWQAP               "AT+CWQAP"
+
+#define CWSAP_ARG(...)                  \
+        (__VA_ARGS__ a0)->ssid,         \
+        (__VA_ARGS__ a0)->pwd,          \
+        (__VA_ARGS__ a0)->channel,      \
+        (__VA_ARGS__ a0)->ecn,          \
+        (__VA_ARGS__ a0)->maxConnect,   \
+        (__VA_ARGS__ a0)->isSsidHidden
+#define SET_CWSAP               "AT+CWSAP=%s,%s,%d,%d%+d%+d", CWSAP_ARG(&)
+#define ASK_CWSAP               "AT+CWSAP?"
+#define GET_CWSAP               "+CWSAP:%s,%s,%d,%d,%d,%d", CWSAP_ARG()
+
+#define ASK_CWLIF               "AT+CWLIF?"
+#define GET_CWLIF               "+CWLIF:%i,%m", a0.ip, a0.mac
+
+#define SET_CWDHCP              "AT+CWDHCP=%d,%d", a0, a1
+#define ASK_CWDHCP              "AT+CWDHCP?"
+#define GET_CWDHCP              "+CWDHCP:%d", a0
+
+#define CWDHCPS_ARG(...)                \
+        (__VA_ARGS__ a0)->leaseMinute,  \
+        (__VA_ARGS__ a0)->startIp,      \
+        (__VA_ARGS__ a0)->endIp
+
+#define SET_CWDHCPS_DISABLE     "AT+CWDHCPS=0"
+#define SET_CWDHCPS             "AT+CWDHCPS:1,%d,%i,%i", CWDHCPS_ARG(&)
+#define ASK_CWDHCPS             "AT+CWDHCPS?"
+#define GET_CWDHCPS             "+CWDHCPS:%d,%i,%i", CWDHCPS_ARG()
+
+#define SET_CWAUTOCONN          "AT+CWAUTOCONN=%d", a0
+#define SET_CWSTARTSMART        "AT+CWSTARTSMART=%d", a0
+#define SET_CWSTOPSMART         "AT+CWSTOPSMART"
+#define SET_WPS                 "AT+WPS=%d", a0
+#define SET_CWHOSTNAME          "AT+CWHOSTNAME=%s", a0
+#define ASK_CWHOSTNAME          "AT+CWHOSTNAME?"
+#define GET_CWHOSTNAME          "+CWHOSTNAME:%s", a0
+#define SET_MDNS_DISABLE        "AT+MDNS=0"
+#define SET_MDNS                "AT+MDNS=1,%s,%s,%d", a0, String("_") + a1, a2
+
+extern String readLine();
+
+CMD(atWifiMode, int32_t a0)
+    tx(SET_CWMODE);
 $
-CMD(atWifiMode, int32_t * mode)
-    tx(AT CWMODE ASK);
-    rx(CWMODE TOKEN, mode);
+CMD(atWifiMode, int32_t * a0)
+    tx(ASK_CWMODE);
+    rx(GET_CWMODE);
 $
 
-CMD(atWifiConnect, 
-    String ssid, 
-    String pwd, 
-    String bssid)
-    if (bssid == ""){
-        tx(AT CWJAP SET, ssid, pwd);
-    }
-    else{
-        tx(AT CWJAP SET, ssid, pwd, bssid);
-    }
+CMD(atWifiConnect, WifiToken const & a0)
+    tx(SET_CWJAP);
+$
+
+CMD(atWifiConnect, WifiLinkedAp * a0)
+    tx(ASK_CWJAP);
+    tx(GET_CWJAP);
 $
 
 CMD(atWifiScan, std::function<void (WifiLinkInfo &)> && call)
     String current;
-    tx(AT CWLAP);
+    tx(ASK_CWLAP);
     while((current = readLine()).length() == 0){
         delay(1);
     }
-    while (current.indexOf(CWLAP TOKEN) != -1){
+    auto lastIndexOf = [](const char * line, char chr, size_t times){
+        for (int i = strlen(line); i-- > 0; ){
+            if (line[i] != chr){
+                continue;
+            }
+            if (--times == 0){
+                return i;
+            }
+        }
+        return -1;
+    };
 
+    while (current.indexOf(GET_CWLAP) != -1){
         //char    line[] = "+CWLAP:(3,\"POT\",-AL00a\",-53,\"12:34:56:78:9a:bc\",1)";
         char *  line = (char *)current.c_str();
-        char *  p = NULL;
+        char *  p = nullptr;
         char *  t;
         int32_t mac[6]; //need 32bit when use sscanf
         int32_t ecn;
         int32_t rssi;
         int32_t channel;
 
-        p = strrchr(line, ',');
-        p[0] = '\0';
+        p = line + lastIndexOf(line, ',', 3);
 
-        for (size_t i = 0, length = strlen(line); length-- > 0; ) {
-            t = p;
-            p = strrchr(line, ',');
-            p[0] = '\0';
-            t[0] = ',';
-        }
-        sscanf(line, CWLAP TOKEN "(%d,", & ecn);
+        sscanf(line, GET_CWLAP "(%d,", & ecn);
         sscanf(p + 1, "%d,\"%x:%x:%x:%x:%x:%x\",%d", 
             & rssi,
             & mac[0],
@@ -89,135 +128,76 @@ CMD(atWifiScan, std::function<void (WifiLinkInfo &)> && call)
 $
 
 CMD(atWifiDisconnect)
-    tx(AT CWQAP);
+    tx(SET_CWQAP);
 $
 
-CMD(atWifiApConfigure, WifiApConfigure * configure)
-    tx(AT CWSAP ASK);
-    rx(CWSAP TOKEN, 
-        & configure->ssid,
-        & configure->pwd,
-        & configure->channel,
-        & configure->ecn,
-        & configure->maxConnect,
-        & configure->isSsidHidden
-    );
+CMD(atWifiApConfigure, WifiApConfigure const & a0)
+    tx(SET_CWSAP);
 $
 
-CMD(atWifiApConfigure, WifiApConfigure const & configure)
-    tx(AT CWSAP SET,
-        configure.ssid,
-        configure.pwd,
-        configure.channel,
-        configure.ecn,
-        more
-    );
-    if (configure.maxConnect == -1){
-        tx();
-    }
-    else if (configure.isSsidHidden == -1){
-        tx(configure.maxConnect);
-    }
-    else{
-        tx(configure.maxConnect, configure.isSsidHidden);
-    }
+CMD(atWifiApConfigure, WifiApConfigure * a0)
+    tx(ASK_CWSAP);
+    rx(GET_CWSAP);
 $
 
 CMD(atWifiUserList, std::function<void (WifiUserList &)> && call)
-    int32_t ip[4];
-    hex_t   mac[6];
-    tx(AT CWLIF);
-    while (rx(CWLIF TOKEN, 
-        ip + 0, '.', ip + 1, '.', ip + 2, '.', ip + 3,
-        mac + 0, ':', mac + 1, ':', mac + 2, ':', mac + 3, ':', mac + 4, ':', mac + 5
-    ) == success){
-        WifiUserList user;
-        copy(user.ip, ip, 4);
-        copy(user.mac, mac, 6);
-        call(user);
+    WifiUserList a0;
+    for (tx(ASK_CWLIF); rx(GET_CWLIF) == success; ){
+        call(a0);
     }
 $
 
-CMD(atDhcp, bool enable, int32_t mask)
-    tx(AT CWDHCP SET, enable, mask);
+CMD(atDhcp, bool a0, int32_t a1)
+    tx(SET_CWDHCP);
 $
 
-CMD(atDhcp, int32_t * result)
-    tx(AT CWDHCP ASK);
-    rx(CWDHCP TOKEN, result);
+CMD(atDhcp, int32_t * a0)
+    tx(ASK_CWDHCP);
+    rx(GET_CWDHCP);
 $
 
 CMD(atDhcpIpRangeClear)
-    tx(AT CWDHCPS SET, disable);
+    tx(SET_CWDHCPS_DISABLE);
 $
 
-CMD(atDhcpIpRange, IpRange const & configure)
-    //xxx.xxx.xxx.xxx -> 12 + 1 bytes
-    char startIp[12 + 1];
-    char endIp[12 + 1];
-    sprintf(startIp, "%d.%d.%d.%d", 
-        configure.startIp[0], 
-        configure.startIp[1], 
-        configure.startIp[2], 
-        configure.startIp[3]
-    );
-    sprintf(endIp, "%d.%d.%d.%d", 
-        configure.endIp[0], 
-        configure.endIp[1], 
-        configure.endIp[2], 
-        configure.endIp[3]
-    );
-    tx(AT CWDHCPS SET, enable, configure.leaseMinute, startIp, endIp);
+CMD(atDhcpIpRange, IpRange const & a0)
+    tx(SET_CWDHCPS);
 $
 
-CMD(atDhcpIpRange, IpRange * configure)
-    int32_t startIp[4];
-    int32_t endIp[4];
-    tx(AT CWDHCPS ASK);
-    if (rx(CWDHCPS TOKEN,
-        & configure->leaseMinute, 
-        & startIp[0], '.',
-        & startIp[1], '.',
-        & startIp[2], '.',
-        & startIp[3],
-        & endIp[0], '.',
-        & endIp[1], '.',
-        & endIp[2], '.',
-        & endIp[3]) == success){
-        copy(configure->startIp, startIp, 4);
-        copy(configure->endIp, endIp, 4);
-    }
+CMD(atDhcpIpRange, IpRange * a0)
+    tx(ASK_CWDHCPS);
+    rx(GET_CWDHCPS);
 $
 
-CMD(atApAutoConnect, bool enable)
-    tx(AT CWAUTOCONN SET, enable);
+CMD(atApAutoConnect, bool a0)
+    tx(SET_CWAUTOCONN);
 $
 
-CMD(atApStartSmart, int32_t type)
-    tx(AT CWSTARTSMAR SET, type);
+CMD(atApStartSmart, int32_t a0)
+    tx(SET_CWSTARTSMART);
 $
 
-CMD(atApStopSmart, int32_t type)
-    tx(AT CWSTOPSMART SET, type);
+CMD(atApStopSmart)
+    tx(SET_CWSTOPSMART);
 $
 
-CMD(atWps, bool enable)
-    tx(AT WPS SET, enable);
+CMD(atWps, bool a0)
+    tx(SET_WPS);
 $
 
-CMD(atHostNameTemp, String const & name)
-    tx(AT CWHOSTNAME SET, name);
+CMD(atHostNameTemp, String const & a0)
+    tx(SET_CWHOSTNAME);
 $
 
-CMD(atHostNameTemp, String * name)
-    tx(AT CWHOSTNAME ASK);
-    rx(CWHOSTNAME TOKEN, name);
+CMD(atHostNameTemp, String * a0)
+    tx(ASK_CWHOSTNAME);
+    rx(GET_CWHOSTNAME);
 $
 
 CMD(atMdnsDisable)
-    tx(AT MDNS SET, disable);
+    tx(SET_MDNS_DISABLE);
 $
 
-CMD(atMdns, String hostName, String serviceName, int32_t port)
-    tx(AT MDNS SET, enable, hostName, String("_") + serviceName, port);
+CMD(atMdns, String a0, String a1, int32_t a2)
+    tx(SET_MDNS);
 $
