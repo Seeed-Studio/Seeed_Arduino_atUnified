e4f6250469f57468126eb4770a5674653c05230c
PowerfulCat <x4495@outlook.com>
2019-11-13 18:55:14 +0800

    Use the %b placeholder to get byte from response line.

--- UnifiedAtBase.cpp
+++ UnifiedAtBase.cpp
@@ -2,15 +2,15 @@
 
 #define SET_RST         "AT+RST"
 #define GET_GMR         "+GMR:%s\n%+GMR:%s\n%+GMR:%s\n%+GMR:%s",    \
-                        & info->atVersion,                          \
-                        & info->sdkVersion,                         \
-                        & info->compileTime,                        \
-                        & info->binVersion
+                        info->atVersion,                            \
+                        info->sdkVersion,                           \
+                        info->compileTime,                          \
+                        info->binVersion
 #define ASK_GMR         "AT+GMR?"
 #define SET_GSLP        "AT+GSLP=%d", ms
 #define SET_SLEEP       "AT+SLEEP=%d", mode
 #define ASK_SLEEP       "AT+SLEEP?"
-#define GET_SLEEP       "+SLEEP:%d", & mode
+#define GET_SLEEP       "+SLEEP:%b", mode
 
 #define UART_ARG(...)   (__VA_ARGS__ config)->rate,                 \
                         (__VA_ARGS__ config)->databits,             \
@@ -27,7 +27,7 @@
 #define GET_UART_DEF    "+UART_DEF:%d,%d,%d,%d,%d",   UART_ARG()
 
 #define ASK_SYSRAM      "AT+SYSRAM?"
-#define GET_SYSRAM      "+SYSRAM:%d", a0
+#define GET_SYSRAM      "+SYSRAM:%d", bytes
 
 #define SYSFLASH    
 #define FS          
@@ -76,14 +76,17 @@ CMD(atUartNovolatile, AtUartConfig * config)
     tx(GET_UART_DEF);
 $
 
+CMD(atRam, int32_t * bytes)
+    tx(ASK_SYSRAM);
+    rx(GET_SYSRAM, bytes);
+$
+
 CMD(atSleepMode, bool mode)
     tx(SET_SLEEP);
 $
 
-CMD(atSleepMode, bool * result)
-    int32_t mode;
+CMD(atSleepMode, bool * mode)
     tx(SET_SLEEP);
     rx(GET_SLEEP);
-    result[0] = mode;
 $
 
