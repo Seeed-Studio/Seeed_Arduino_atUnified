cdbbac8202815971d7672f6a72d351e221d984c7
PowerfulCat <x4495@outlook.com>
2019-11-26 09:28:43 +0800

    Add some useful tools for general purpose.

---
+++ Utilities.h
@@ -0,0 +1,26 @@
+#pragma once
+#include"stdint.h"
+
+template<class type>
+constexpr int32_t indexOfSetBit(type value){
+    static_assert(type(-1) > 0, "type need match unsigned type");
+    int32_t index = 0;
+    type v = value;
+
+    for (type c = 16, mask = 0; ; c >>= 1) {
+        mask = (1 << c) - 1;
+        if (!(value & mask)){
+            value >>= c;
+            index += c;
+        }
+        if (c == 0){
+            break;
+        }
+    }
+    return v & (1ull << index) ? index : -1;
+}
+
+template<class type>
+constexpr int32_t indexOfResetBit(type value){
+    return indexOfSetBit(~value);
+}
